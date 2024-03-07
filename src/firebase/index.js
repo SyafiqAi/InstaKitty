9690b4d4208bda7994067285b800db9861322020
Syafiq <isyafiq@gmail.com>
2024-03-07 12:40:26 +0800

    add firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC52a-9sjbxm0-XlEv5S_nTz4iMzE0w7B4",
+  authDomain: "insta-kitty.firebaseapp.com",
+  projectId: "insta-kitty",
+  storageBucket: "insta-kitty.appspot.com",
+  messagingSenderId: "744307421922",
+  appId: "1:744307421922:web:6d91eb7e312da16c2564bb",
+  measurementId: "G-K3346XPPH0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
